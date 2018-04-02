2725dc2a192b0af72f3080c96a1cca693c3f8836
msi <1635941948@qq.com>
2018-04-02 10:59:38 +0800

    2018.4.2  框架

---
+++ src/main/java/com/yingchuang/AnswersystemApplication.java
@@ -0,0 +1,12 @@
+package com.yingchuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnswersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnswersystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yingchuang/AnswersystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yingchuang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnswersystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
