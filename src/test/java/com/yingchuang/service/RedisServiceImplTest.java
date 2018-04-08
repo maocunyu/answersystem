4493a9ed6366f51bc849b7a40057b3dc5d5c553e
msi <1635941948@qq.com>
2018-04-08 15:18:46 +0800

    2018.4.8

--- src/test/java/com/yingchuang/service/RedisServiceImplTest.java
+++ src/test/java/com/yingchuang/service/RedisServiceImplTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
